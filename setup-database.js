e5547bf487646771a2bcff3ff5328b400ba3dbe1
unknown <foyekeoyediran@gmail.com>
2025-08-08 21:16:36 +0100

    changes

--- setup-database.js
+++ setup-database.js
@@ -6,10 +6,10 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const dbConfig = {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    host: process.env.mysql.railway.internal,
+    user: process.env.root,
+    password: process.env.yBGPBQAcosjwmShEfljuPDXHKegcJbMV,
+    database: process.env.railway,
     charset: 'utf8mb4',
     multipleStatements: true
 };
