e77cb8dd1772d6d72189fe7f370d8894d4a88dfe
unknown <foyekeoyediran@gmail.com>
2025-11-02 19:47:42 +0100

    Fix trust proxy + DB pooling

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Trust Railway proxy - ADD THIS LINE
-app.set('trust proxy', true);
+app.set('trust proxy', 1);
 
 // Middleware
 app.use(helmet());
